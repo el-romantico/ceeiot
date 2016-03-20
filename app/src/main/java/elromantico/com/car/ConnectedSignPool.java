371fb42c2cfec914f5645b0c0c38878bd4bc6cb3
angelvenchev <angelvenchev@yahoo.com>
2016-03-20 07:34:04 +0200

    Order currently connected signs by timestamp descending

--- app/src/main/java/elromantico/com/car/ConnectedSignPool.java
+++ app/src/main/java/elromantico/com/car/ConnectedSignPool.java
@@ -1,7 +1,11 @@
 package elromantico.com.car;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -14,8 +18,17 @@ public class ConnectedSignPool {
         sign.insertInPool(this);
     }
 
-    public ArrayList<ConnectedSign> toOrderedSignList() {
-        // TODO: Sort by timestamp
-        return new ArrayList<>(connectedDevices);
+    public List<ConnectedSign> toOrderedSignList() {
+        List<ConnectedSign> orderedSignList = new ArrayList<>(connectedDevices);
+
+        Collections.sort(orderedSignList, new Comparator<ConnectedSign>() {
+
+            @Override
+            public int compare(ConnectedSign lhs, ConnectedSign rhs) {
+                return new Long(rhs.timestamp).compareTo(lhs.timestamp);
+            }
+        });
+
+        return orderedSignList;
     }
 }
