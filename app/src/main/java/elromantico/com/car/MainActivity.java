95627c1b66ff64f1ef5e3b98fddc9af770405f6c
dsmilyanov <dsmilyanov@gmail.com>
2016-03-20 02:25:28 +0200

    Resolve conflicts

--- app/src/main/java/elromantico/com/car/MainActivity.java
+++ app/src/main/java/elromantico/com/car/MainActivity.java
@@ -1,14 +1,25 @@
 package elromantico.com.car;
 
+import android.animation.Animator;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Fade;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.apache.commons.collections.buffer.CircularFifoBuffer;
 
@@ -27,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
 
     private static Set<String> whitelist = new HashSet<>();
+
     static {
         whitelist.add("C0:EE:FB:58:B1:DE");
         whitelist.add("10:68:3F:58:81:50");
@@ -73,6 +85,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,5 +116,29 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_ENABLE_BT);
         }
         bluetoothAdapter.startDiscovery();
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        ImageView bigSign = (ImageView) findViewById(R.id.bigSign);
+
+        int cx = bigSign.getWidth() / 2;
+        int cy = bigSign.getHeight() / 2;
+        float finalRadius = (float) Math.hypot(cx, cy);
+
+        Animator animator = ViewAnimationUtils.createCircularReveal(bigSign, cx, cy, 0, finalRadius);
+
+        bigSign.setVisibility(View.VISIBLE);
+        animator.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(bluetoothReceiver);
+
+        super.onDestroy();
     }
 }
