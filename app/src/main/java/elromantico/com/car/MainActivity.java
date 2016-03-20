fcb152e3e3f234bd96b94e2b4b1aeefde9ea2e3a
dsmilyanov <dsmilyanov@gmail.com>
2016-03-20 03:21:45 +0200

    Show signs realtime

--- app/src/main/java/elromantico/com/car/MainActivity.java
+++ app/src/main/java/elromantico/com/car/MainActivity.java
@@ -1,24 +1,16 @@
 package elromantico.com.car;
 
-import android.animation.Animator;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.Fade;
-import android.view.View;
-import android.view.ViewAnimationUtils;
-import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.apache.commons.collections.buffer.CircularFifoBuffer;
@@ -30,7 +22,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,16 +32,21 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
 
     private static Set<String> whitelist = new HashSet<>();
+    private static final Map<String, Integer> SIGNS = new HashMap<>();
 
     static {
         whitelist.add("C0:EE:FB:58:B1:DE");
         whitelist.add("10:68:3F:58:81:50");
         whitelist.add("F0:08:F1:F0:52:71");
+
+        SIGNS.put("A12", R.drawable.alpha12);
+        SIGNS.put("B2", R.drawable.beta2);
     }
 
     private Location location;
     private Direction direction;
     private CircularFifoBuffer locations = new CircularFifoBuffer(1000);
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     private GoogleMap mMap;
     private TextView signView;
@@ -73,19 +72,23 @@ public class MainActivity extends AppCompatActivity {
 
                     LatLng markerPosition = new LatLng(latitude, longitude);
                     mMap.addMarker(new MarkerOptions().position(markerPosition));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(markerPosition));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 15.0f));
 
                     locations.add(location);
-                    locationView.setText("Location: " + location.toString());
 
                     Object[] locationsObj = locations.toArray();
                     if (locationsObj.length > 1) {
-                        speedView.setText("Speed: " + SpeedCalculator.calculateSpeed((Location) locationsObj[locationsObj.length - 2],
-                                (Location) locationsObj[locationsObj.length - 1]));
+                        locationView.append("Speed: " + SpeedCalculator.calculateSpeed((Location) locationsObj[locationsObj.length - 2],
+                                (Location) locationsObj[locationsObj.length - 1]) + "\n");
                     } else {
-                        speedView.setText("Speed: Only one!");
+                        locationView.append("Speed: Only one!\n");
                     }
+
+                    ImageView bigSign = (ImageView) findViewById(R.id.bigSign);
+                    bigSign.setImageDrawable(getDrawable(SIGNS.get(signName)));
                 }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                bluetoothAdapter.startDiscovery();
             }
         }
     };
@@ -110,12 +113,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        signView = (TextView) findViewById(R.id.sign);
+        //signView = (TextView) findViewById(R.id.sign);
         locationView = (TextView) findViewById(R.id.location);
-        speedView = (TextView) findViewById(R.id.speed);
+        //speedView = (TextView) findViewById(R.id.speed);
 
         registerReceiver(bluetoothReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        registerReceiver(bluetoothReceiver, new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+
         if (!bluetoothAdapter.isEnabled()) {
             startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_ENABLE_BT);
         }
@@ -123,21 +127,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-
-        ImageView bigSign = (ImageView) findViewById(R.id.bigSign);
-
-        int cx = bigSign.getWidth() / 2;
-        int cy = bigSign.getHeight() / 2;
-        float finalRadius = (float) Math.hypot(cx, cy);
-
-        Animator animator = ViewAnimationUtils.createCircularReveal(bigSign, cx, cy, 0, finalRadius);
-
-        bigSign.setVisibility(View.VISIBLE);
-        animator.start();
-    }
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//
+//        ImageView bigSign = (ImageView) findViewById(R.id.bigSign);
+//
+//        int cx = bigSign.getWidth() / 2;
+//        int cy = bigSign.getHeight() / 2;
+//        float finalRadius = (float) Math.hypot(cx, cy);
+//
+//        Animator animator = ViewAnimationUtils.createCircularReveal(bigSign, cx, cy, 0, finalRadius);
+//
+//        bigSign.setVisibility(View.VISIBLE);
+//        animator.start();
+//    }
 
     @Override
     protected void onDestroy() {
