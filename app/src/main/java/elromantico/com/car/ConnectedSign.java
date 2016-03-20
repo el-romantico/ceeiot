a455ccc4dda6da8431811f46038d8a49703425f2
angelvenchev <angelvenchev@yahoo.com>
2016-03-20 05:56:08 +0200

    Add connected sign class

---
+++ app/src/main/java/elromantico/com/car/ConnectedSign.java
@@ -0,0 +1,30 @@
+package elromantico.com.car;
+
+/**
+ * Created by angel on 3/20/2016.
+ */
+public class ConnectedSign {
+    public long timestamp;
+    public String signName;
+
+    public ConnectedSign(String signName, long timestamp) {
+        this.signName = signName;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ConnectedSign)) return false;
+
+        ConnectedSign that = (ConnectedSign) o;
+
+        return signName.equals(that.signName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return signName.hashCode();
+    }
+}
